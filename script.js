d4db75011d3f39ce817f015f0b84a281f23335d0
Schulzkafer <gatsby886@gmail.com>
2020-09-25 23:48:39 -0300

    ok2

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ for (let elem of tds) {
 }
 
 let myNums = [];
-
+ 
 function marcar (el) {
   event.preventDefault();
   if (el.target.classList.contains('cruz')) {
@@ -38,17 +38,23 @@ function marcar (el) {
 
 }
 
-let attempts = 1;
+
 
 function rot () {
+  console.log('ddddddddddddd')
+  if (document.querySelector('.reiniciar').style.visibility == 'visible') {
+    document.querySelector('.reiniciar').style.visibility = 'hidden';
+  }
 document.querySelector('img').classList.add('rotate');
-document.querySelector('#fala').innerHTML='Espera, tô calculando...'
+document.querySelector('#fala').innerHTML='Espera, tô calculando...';
+document.querySelector('#tecla').style.visibility = 'hidden';
 setTimeout(()=>calc(),0);
 }
 
 function calc () {
-  myNums = myNums.map(x=>Number(x));
-  function getRandomIntInclusive(min, max) {
+  let attempts = 1;
+   myNums = myNums.map(x=>Number(x));
+    function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
@@ -56,8 +62,6 @@ function calc () {
 
 let numsRepeat = [];
 while (numsRepeat.length < 6) {
-  if (attempts % 5000000 == 0) {
-    }
   let n = getRandomIntInclusive(1, 60)
   if (!myNums.includes(n) ) {
     numsRepeat = [];
@@ -70,17 +74,22 @@ while (numsRepeat.length < 6) {
 }
 
 document.querySelector('img').classList.remove('rotate');
-let att = attempts.toString().slice(0, -6) + '.' + attempts.toString().slice(-6, -3) + '.' + attempts.toString().slice(-3);
-  let anosModern = Math.round(attempts/365).toString();
-  let anos = (anosModern.length>6) ? anosModern.slice(0, -6) + '.' + anosModern.toString().slice(-6, -3) + '.' + anosModern.toString().slice(-3) : (anosModern.length > 3) ? anosModern.slice(0, -3) + '.' + anosModern.toString().slice(-3) : anosModern;
 
-  let anosDeVidaModern = Math.round(attempts/32850).toString();
-  let anosDeVida = (anosDeVidaModern.length>6) ? anosDeVidaModern.slice(0, -6) + '.' + anosDeVidaModern.toString().slice(-6, -3) + '.' + anosDeVidaModern.toString().slice(-3) : (anosDeVidaModern.length > 3) ? anosDeVidaModern.slice(0, -3) + '.' + anosDeVidaModern.toString().slice(-3) : anosDeVidaModern;
+let attModern = attempts.toString();
+let att = (attModern > 6) ? attempts.toString().slice(0, -6) + '.' + attempts.toString().slice(-6, -3) + '.' + attempts.toString().slice(-3) : (attModern > 3) ? attempts.toString().slice(0, -3) + '.' + attModern.toString().slice(-3) : attModern;
+  
+  let anosModern = Math.round(attempts / 365).toString();
+  let anos = (anosModern.length > 6) ? anosModern.slice(0, -6) + '.' + anosModern.toString().slice(-6, -3) + '.' + anosModern.toString().slice(-3) : (anosModern.length > 3) ? anosModern.slice(0, -3) + '.' + anosModern.toString().slice(-3) : anosModern;
+
+  let anosDeVidaModern = Math.round(attempts / 32850).toString();
+  let anosDeVida = (anosDeVidaModern.length > 6) ? anosDeVidaModern.slice(0, -6) + '.' + anosDeVidaModern.toString().slice(-6, -3) + '.' + anosDeVidaModern.toString().slice(-3) : (anosDeVidaModern.length > 3) ? anosDeVidaModern.slice(0, -3) + '.' + anosDeVidaModern.toString().slice(-3) : anosDeVidaModern;
 
   let estatistica = `Parabéns!<br>Você ganharia essa loteria na ${att}<sup>a</sup> vez.
   <br>Apostando uma vez cada dia, isso demoraria ${anos} anos.<br>Para ganhá-la uma vez durante 90 anos de vida, você precisaria apostar ${anosDeVida} vezes cada dia.<br> P.S.: a probabilidade média de ganhar a loteria é um em 50 milhões.`;
 
   document.querySelector('#fala').innerHTML = estatistica;
+  document.querySelector('.reiniciar').style.visibility = 'visible';
+  document.querySelector('#tecla').style.visibility = 'visible';
 return attempts;
 }
 
